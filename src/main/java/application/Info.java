e669c90293720221389b2504c8921daf509a345f
Peter <peter@9.112.73.238>
2019-05-31 13:37:44 +0800

    update 1

--- src/main/java/application/Info.java
+++ src/main/java/application/Info.java
@@ -9,9 +9,9 @@ public class Info {
 
   @EventListener(ApplicationReadyEvent.class)
     public void contextRefreshedEvent() {
-      System.out.println("The following endpoints are available by default :-");
-      System.out.println("  Health        : http://localhost:8080/health");
-      System.out.println("  Application   : http://localhost:8080/v1/");
+      	System.out.println("The following endpoints are available by default :-");
+      	System.out.println("  Health        : http://localhost:8080/health");
+      	System.out.println("  Application   : http://localhost:8080/v1/");
     }
 
 }
